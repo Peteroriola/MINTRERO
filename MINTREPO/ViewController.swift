934b42e02f805b29aefa728441f6af21f61d1b82
Peter <peoriola@unionbankng.com>
2020-11-11 16:15:42 +0100

    Initial Commit

---
+++ MINTREPO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MINTREPO
+//
+//  Created by UNION on 11/11/2020.
+//  Copyright © 2020 Peter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
